1ca235cbee45f0d46c6e42bbd45f3a79c6922253
RG1208 <rachitgarg1208@gmail.com>
2025-06-17 09:09:44 +0000

    runtime

--- AgriOracleBackend/build.sh
+++ AgriOracleBackend/build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
+
+# Explicitly install Python 3.9
+pyenv install 3.9.18 -s
+pyenv global 3.9.18
 
 pip install --upgrade pip
 pip install -r requirements.txt
